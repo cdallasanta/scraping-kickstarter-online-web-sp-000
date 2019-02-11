ea221ab63db60663e572cc5505ef49619b990704
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-02-11 23:59:44 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,10 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
+  projects = {}
+
+  
+
   #all projects: kickstarter.css("li.project.grid_4").first
   #title: .css("h2.bbcard_name strong a").text
   #img: .css(".project-thumbnail a img").attribute("src").value
