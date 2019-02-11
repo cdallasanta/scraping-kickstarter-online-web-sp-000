bd90bcaa8f73bde8aea550a9a0248b1bd3d65565
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-02-11 23:49:37 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,7 @@ def create_project_hash
 
   #all projects: kickstarter.css("li.project.grid_4").first
   #title: .css("h2.bbcard_name strong a").text
-  #img: 
+  #img: .css("")
 
   binding.pry
 end
