64f6373dd9d1ee7d94d52e35725eb3af91c664c4
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-02-11 23:55:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,7 @@ def create_project_hash
   #img: .css(".project-thumbnail a img").attribute("src").value
   #desc: .css("p").text
   #location: .css("span.location-name").text
-  #percent: .css(".project-stats ")
+  #percent: .css(".project-stats li.first-funded strong").text
 
 
   binding.pry
